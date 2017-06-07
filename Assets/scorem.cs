af259b4a06b271145094ba5f8bf3d52e24470dff
浜谷辰法 <collagecollege0@gmail.com>
2017-06-07 22:24:40 +0900

    ピンボールのコミット

---
+++ Assets/scorem.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bascket : MonoBehaviour {
+
+	public ScoreText scoreText;　//外部のScoteTextオブジェクトを見えるよう定義
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+	void OnCollisionEnter(Collision collision)
+	{
+		if (collision.gameObject.tag == "ball") {
+			scoreText.GetComponent<ScoreText>().score++;
+		}
+	}
+}
